5504d8170d6e10bb6644ce198563df94c69a79db
U-Aleksandra <s.ushkva@yandex.ru>
2023-02-14 21:54:45 +0400

    изменения

--- Диплом/дипломная_клиент/graduate_work/graduate_work/graduate_work/PageRegist.xaml.cs
+++ Диплом/дипломная_клиент/graduate_work/graduate_work/graduate_work/PageRegist.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace graduate_work
 {
@@ -17,16 +18,23 @@ namespace graduate_work
 			InitializeComponent ();
 		}
 
-        private void buttonRegist_Clicked(object sender, EventArgs e)
+        bool next1;
+        bool next2;
+        bool next3;
+        private async void buttonRegist_Clicked(object sender, EventArgs e)
         {
+            
+
             if ((string.IsNullOrEmpty(entryName.Text)) ||
 			(string.IsNullOrWhiteSpace(entryName.Text)))
 			{
 				errorName.Text = "Введите имя";
+                next1 = false;
 			}
 			else
 			{
 				errorName.Text = " ";
+                next1 = true;
 			}
 
             if ((string.IsNullOrEmpty(phoneEntry.Text)) ||
@@ -37,10 +45,12 @@ namespace graduate_work
             else if (phoneEntry.Text.Length < 16)
             {
                 errorPhone.Text = "Неправильный формат";
+                next2 = false;
             }
             else
             {
                 errorPhone.Text = " ";
+                next2= true;
             }
 
             if ((string.IsNullOrEmpty(passwordEntry.Text)) ||
@@ -49,22 +59,27 @@ namespace graduate_work
                 errorPassword.TextColor = Color.Red;
                 errorPassword.Text = "Введите пароль";
             }
+
+            if (next1 && next2 && next3 && checkBoxSpecial.IsChecked == true)
+            {
+                await Navigation.PushAsync(new PageRegistSpecialist());
+            }
+           
+        }
+        private void passwordEntryReturn_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (passwordEntryReturn.Text != passwordEntry.Text)
+            {
+                errorPassword.TextColor = Color.Red;
+                errorPassword.Text = "Пароли не совпадают";
+                next3 = false;
+            }
             else
             {
-                errorPassword.Text = " ";
-                if (passwordEntryReturn.Text != passwordEntry.Text)
-                {
-                    errorPassword.TextColor = Color.Red;
-                    errorPassword.Text = "Пароли не совпадают";
-                }
-                else
-                {
-                    errorPassword.TextColor = Color.Green;
-                    errorPassword.Text = "Пароли совпадают";
-                }
+                errorPassword.TextColor = Color.Green;
+                errorPassword.Text = "Пароли совпадают";
+                next3 = true;
             }
-
-           
         }
     }
 }
