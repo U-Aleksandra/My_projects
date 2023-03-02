223326204a1cbc294468a6a6ffb35953af756258
U-Aleksandra <s.ushkva@yandex.ru>
2023-03-03 00:05:06 +0400

    изменения

---
+++ Диплом/дипломная_клиент/graduate_work/graduate_work/graduate_work/apiConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+
+namespace graduate_work.Droid
+{
+    public static class apiConfig
+    {
+        public static HttpClientHandler clientHandler = new HttpClientHandler();
+        public static HttpClient client;
+
+        static apiConfig()
+        {
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+            client = new HttpClient(clientHandler);
+        }
+    }
+}
